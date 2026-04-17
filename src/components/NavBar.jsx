53d6b5ee3d401b09592fdec7e8b75b344edfccba
Oluwabukunmi Akinbisola <preciousbukunmi07@gmail.com>
2026-04-17 20:36:18 +0100

    updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,21 +2,20 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav className="flex justify-between px-6 py-4 items-center border-gray-200">
-      <h1 className="text-3xl text-gray-800">Lizzy Fashion</h1>
-      <ul className="flex uppercase tracking-wide gap-4">
-        <li className="flex hover:text-black">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="flex hover:text-black">
-          <Link to="/collections">Collections</Link>
-        </li>
-        <li className="flex hover:text-black">
-          <Link to="/about">About</Link>
-        </li>
-        <li className="flex hover:text-black">
-          <Link to="/contact">Contact</Link>
-        </li>
+    <nav className="flex justify-between px-6 py-4 items-center border-gray-200 fixed top-0 left-0 w-full bg-white z-50">
+      <h1 className="text-3xl text-gray-800 font-semibold">Lizzy Fashion</h1>
+      <ul className="flex uppercase tracking-wide gap-8">
+        {["Home", "Collections", "About", "Contact"].map((item) => (
+          <li key={item} className="relative group">
+            <Link
+              to={item === "Home" ? "/" : `/${item.toLowerCase()}`}
+              className="text-gray-600 hover:text-black transition-colors duration-500 pb-1"
+            >
+              {item}
+              <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-gray-800 transition-all duration-500 group-hover:w-full"></span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import Hero from "../components/Hero";
 import FeaturedCollections from "../components/FeaturedCollections";
+import React from "react";
+import AboutSnippet from "../components/LearnMore";
 
 function Home() {
   return (
     <div>
       <Hero />
       <FeaturedCollections />
+      <AboutSnippet />
     </div>
   );
 }
